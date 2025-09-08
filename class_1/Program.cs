5bf93869c6a1d777ee7208a748e681c0c41ac72e
ghazalasehar <ghazalasehar333@gmail.com>
2025-09-08 17:54:10 +0500

    printing writeline or write

---
+++ class_1/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+Console.WriteLine("welcome to c# programming ");//print data
+Console.Write("hello world");
+Console.WriteLine(".net frame work");
+
+Console.WriteLine("Enter your first name: ");
+string firstName = Console.ReadLine();
+
+Console.WriteLine("Enter your second name: ");
+string lastName = Console.ReadLine();
+
+//Console.WriteLine("My name is: "+firstName);
+
+Console.WriteLine($"my name is : {firstName} {lastName}");
+
+
+
+Console.ReadKey();//Read console data
